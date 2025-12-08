f4433ffb5bc95188d50d77cff6f7866142ad1bfa
JacktheGeat <jacklynch706@gmail.com>
2025-12-08 13:41:08 -0500

    fixed maybe?

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client'; // or 'react-dom' for older versions
 import { BrowserRouter } from 'react-router-dom';
-import './index.css'
-import App from './App.jsx'
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(
-      <React.StrictMode>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </React.StrictMode>
-    );
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
