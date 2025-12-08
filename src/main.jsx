e0b66aca8dcd80791bcd1d8cc021e09ff28ac3c8
JacktheGeat <jacklynch706@gmail.com>
2025-12-08 15:18:04 -0500

    changed hashrouter to browserrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
         {/* Add other routes here */}
       </Routes>
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
