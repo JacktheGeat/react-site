33aaae752e1f025a9bd4bb5f0ecac815223ee130
JacktheGeat <jacklynch706@gmail.com>
2025-12-08 14:48:44 -0500

    i think this works

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client'; // or 'react-dom' for older versions
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+import ReactDOM from 'react-dom/client';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        {/* Add other routes here */}
+      </Routes>
+    </Router>
   </React.StrictMode>
 );
